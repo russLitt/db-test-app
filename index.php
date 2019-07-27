bcfbde6b7e3001a4224e25817b5d9de5653c62af
Russell Anderson <russellanderson2000@gmail.com>
2019-07-26 23:02:33 -0400

    reformat and add bs components to index

--- index.php
+++ index.php
@@ -1,21 +1,25 @@
-
 <?php include "templates/header.php"; ?>
 
-
-<ul class="mx-auto" style="width: 230px;">
-  <li>
-    <a href="create.php"><strong>Create</strong></a> - add a user
-  </li>
-  <li>
-    <a href="read.php"><strong>Read</strong></a> - find a user
-  </li>
-  <li>
-  <a href="update.php"><strong>Update</strong></a> - edit a user
-    </li>
-    <li>
-    <a href="delete.php"><strong>Delete</strong></a> - delete a user
-  </li>
-</ul>
-
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-md-4"></div>
+    <div class="col-md-4">
+      <ul class="mx-auto" style="width: 230px;">
+        <li>
+          <a href="create.php"><strong>Create</strong></a> - Add a User
+        </li>
+        <li>
+          <a href="read.php"><strong>Read</strong></a> - Find a User
+        </li>
+        <li>
+          <a href="update.php"><strong>Update</strong></a> - Edit a User
+        </li>
+        <li>
+          <a href="delete.php"><strong>Delete</strong></a> - Remove a user
+        </li>
+      </ul>
+    </div>
+  </div>
+</div>
 
 <?php include "templates/footer.php"; ?>
