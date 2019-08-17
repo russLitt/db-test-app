b05054ba34fa63d45a420c7f9bfcd8843d849413
Russell Anderson <russellanderson2000@gmail.com>
2019-08-17 00:07:32 -0400

    WIP: tinkering with jumbotron

--- templates/header.php
+++ templates/header.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html class="h-100" lang="en">
+<html lang="en">
 
 <head>
   <meta charset="utf-8" />
@@ -8,18 +8,14 @@
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <link rel="stylesheet" href="css/style.css" />
 
-  <div style="margin-top: -50px;">
   <title>Simple Database App</title>
-  </div>
 
 </head>
 
-<body class="d-flex flex-column h-100">
-  <div class="container-fluid">
+<body>
 
-    <h1 class="jumbotron text-center" style="margin-top: 25px;">Simple Database App</h1>
+    <h1 class="jumbotron text-center" id="tronjumbo">Simple Database App</h1>
 
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-  </div>
