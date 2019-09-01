bfd74050cfb3aef439d32ef12a7b6557475e32db
Russell Anderson <russellanderson2000@gmail.com>
2019-08-31 21:55:17 -0400

    made index link fonts larger, added some padding

--- index.php
+++ index.php
@@ -1,24 +1,22 @@
 <?php require "templates/header.php"; ?>
 <div class="container-fluid" id="container-main">
   <div class="row justify-content-md-center" id="content-container">
-    <!-- <div class="col-md-4"></div> -->
-    <div class="col-md-auto mx-auto">
-      <ul>
-        <li>
+    <div class="col-md-auto">
+      <ul id="all-categories">
+        <li class="individual-categories">
           <a href="create.php"><strong>Create</strong></a> - Add a User
         </li>
-        <li>
+        <li class="individual-categories">
           <a href="read.php"><strong>Read</strong></a> - Find a User
         </li>
-        <li>
+        <li class="individual-categories">
           <a href="update.php"><strong>Update</strong></a> - Edit a User
         </li>
-        <li>
+        <li class="individual-categories">
           <a href="delete.php"><strong>Delete</strong></a> - Remove a user
         </li>
       </ul>
     </div>
-    <!-- <div class="col-md-4"></div> -->
   </div>
 </div>
   <?php require "templates/footer.php"; ?>
